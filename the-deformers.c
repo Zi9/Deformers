459335845ed7e0f00012e6b083779f3a9db20580
Zi9 <kevin.lumik@gmail.com>
2021-04-04 17:20:07 +0300

    Basic glfw skeleton

--- the-deformers.c
+++ the-deformers.c
@@ -2,6 +2,28 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+GLFWwindow* window;
+
 int main(int argc, char** argv)
 {
+    if (!glfwInit())
+        return -1;
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
 }
