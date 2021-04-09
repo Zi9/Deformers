7870057d367ecad03ddefee059621e78600043e6
Zi9 <kevin.lumik@gmail.com>
2021-04-09 17:47:27 +0300

    Functions separated

--- the-deformers.c
+++ the-deformers.c
@@ -4,14 +4,32 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <stdbool.h>
 
 GLFWwindow* window;
 int win_width, win_height;
+
+void onInput(GLFWwindow* window)
+{
+    if(glfwGetKey(window, GLFW_KEY_CAPS_LOCK) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void onUpdate()
+{
+
+}
+
+void onRender()
+{
+
+}
 
 int main(int argc, char** argv)
 {
     if (!glfwInit())
         return -1;
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
     if (!window)
     {
@@ -19,11 +37,12 @@ int main(int argc, char** argv)
         return -1;
     }
     glfwMakeContextCurrent(window);
+
+    glfwGetFramebufferSize(window, &win_width, &win_height);
+    glViewport(0, 0, win_width, win_height);
     while (!glfwWindowShouldClose(window))
     {
-        glfwGetFramebufferSize(window, &win_width, &win_height);
-        glViewport(0, 0, win_width, win_height);
-        
+        onInput(window);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
