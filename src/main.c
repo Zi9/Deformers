c1d6cbe1d4c523f6e7453bbbf05774beed750d95
Zi9 <kevin.lumik@gmail.com>
2021-04-19 16:17:08 +0300

    Bigger res

--- src/main.c
+++ src/main.c
@@ -1,4 +1,11 @@
 #include "core/config.h"
 #include "game/game.h"
 
-int main() { game_main(config_get_default()); }
+int main()
+{
+    Config cfg = config_get_default();
+    cfg.render.width = 640;
+    cfg.render.height = 360;
+    cfg.render.upscaleMultiplier = 2;
+    game_main(cfg);
+}
